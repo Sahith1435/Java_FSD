117ab436d207dff7d0289481090328b8326c9741
sahit <sahit@LAPTOP-M72K77OT>
2022-02-10 22:56:45 +0530

    Commit 17 - Searching Algos

---
+++ Java_phase1/src/searchingAlgos/ExponentialSearch.java
@@ -0,0 +1,38 @@
+package searchingAlgos;
+
+import java.util.Arrays;
+
+public class ExponentialSearch {
+	public static  void main(String[] args){
+
+	    int[] arr = {10,20,30,40,50};
+	    int length= arr.length;
+	    int value = 50;
+	    int output = exponentialSearch(arr,length,value);
+
+	    if(output<0)
+	    {
+	    	System.out.println( "Element is not present in the array");
+	    }
+	    else 
+	    {
+	    	System.out.println( "Element is  present in the array at index :"+output);
+	    }
+
+	        }
+
+public static int exponentialSearch(int[] arr ,int length, int value ){
+
+	        if(arr[0]==value)
+	        {
+	            return 0;
+	        }
+	        
+	        int i=1;
+	        while(i<length && arr[i]<=value)
+	        {
+	        	i=i*2;
+	        }
+	   return Arrays.binarySearch(arr,i/2,Math.min(i,length),value);
+	        }
+	}
